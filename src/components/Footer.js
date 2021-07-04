0d87ad059b924ebb19ab9c8180a39f8e72e85bf6
Mikhail Kirichkov <mihailkirichkov@yandex.ru>
2021-07-04 18:29:35 +0300

    feat: добавлено Ридми и благодарности

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <div>
       <p className="footer__author">&#169; {new Date().getFullYear()} MSE by Mikhail Kirichkov</p>
+      <p className="footer__gratitude">Created with OMDb API</p>
     </div>
   )
 }
