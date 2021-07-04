07651baef540c4fe944b3b0f755143e9f581de81
Mikhail Kirichkov <mihailkirichkov@yandex.ru>
2021-07-04 22:22:04 +0300

    feat: косметические улучшения

--- src/components/ModalWithInfo.js
+++ src/components/ModalWithInfo.js
@@ -5,15 +5,17 @@ function ModalWithInfo(props) {
 
 
   return (
-    <div className= {`info__wrapper ${props.isOpen ? 'info__wrapper_active' : "" }`}>
-      <button name="closeButton" className="info__close-btn" type="button" onClick={props.onClose}></button>
+    <div onClick={props.onClose} className={`info__wrapper ${props.isOpen ? 'info__wrapper_active' : "" }`}>
       <div className="info">
-        <h2 className="info__caption">Director:</h2>
-        <p name="infoDirector" className="info__text">{props.data.Director}</p>
-        <h2 className="info__caption">Actors:</h2>
-        <p name="infoActors" className="info__text">{props.data.Actors}</p>
-        <h2  className="info__caption">Description:</h2>
-        <p name="infoAbout" className="info__text">{props.data.Plot}</p>
+        <div className="info__text-section">
+          <h2 className="info__caption">Director:</h2>
+          <p name="infoDirector" className="info__text">{props.data.Director}</p>
+          <h2 className="info__caption">Actors:</h2>
+          <p name="infoActors" className="info__text">{props.data.Actors}</p>
+          <h2  className="info__caption">Description:</h2>
+          <p name="infoAbout" className="info__text info__about">{props.data.Plot}</p>
+        </div>
+      <img className="info__poster" src={props.data.Poster} alt={props.data.Title}/>
       </div>
     </div>
   )
