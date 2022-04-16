0f389918d94c20843e84b391f11fa3f61b5d50dc
Mikhail Kirichkov <mihailkirichkov@yandex.ru>
2022-04-16 20:26:53 +0300

    fix/feat: добавлены скриншоты и пофикшена высота приложения

--- src/components/ModalWithInfo.js
+++ src/components/ModalWithInfo.js
@@ -5,17 +5,17 @@ import imageNotFoundPath from "../images/notFound.png"
 function ModalWithInfo(props) {
 
   return (
-    <div onClick={props.onClose} className={`info__wrapper ${props.isOpen ? 'info__wrapper_active' : "" }`}>
+    <div onClick={props.onClose} className={`info__wrapper ${props.isOpen ? 'info__wrapper_active' : ""}`}>
       <div className="info">
+        <img className="info__poster" src={props.data.Poster === "N/A" ? imageNotFoundPath : props.data.Poster} alt={props.data.Title} />
         <div className="info__text-section">
           <h2 className="info__caption">Director:</h2>
           <p name="infoDirector" className="info__text">{props.data.Director}</p>
           <h2 className="info__caption">Actors:</h2>
           <p name="infoActors" className="info__text">{props.data.Actors}</p>
-          <h2  className="info__caption">Description:</h2>
+          <h2 className="info__caption">Description:</h2>
           <p name="infoAbout" className="info__text info__about">{props.data.Plot}</p>
         </div>
-      <img className="info__poster" src={props.data.Poster === "N/A" ? imageNotFoundPath : props.data.Poster} alt={props.data.Title}/>
       </div>
     </div>
   )
