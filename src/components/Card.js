12dddea3af28a6e8037f58abf0d084d561fa8866
Mikhail Kirichkov <mihailkirichkov@yandex.ru>
2022-04-16 23:57:26 +0300

    fix: исправлена заглушка, когда нет постера

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 //ФК, возвращающий разметку карточки с фильмом
 import React from 'react';
-import imageNotFoundPath from "../images/notFound.png"
+import imageNotFoundPath from "../images/notAvailable.svg"
 
 function Card(props) {
 
@@ -10,7 +10,7 @@ function Card(props) {
 
   return (
     <li key={props.movie.imdbID} className="card" >
-      <img src={props.movie.Poster === "N/A" ? imageNotFoundPath : props.movie.Poster} alt={props.movie.Title} className="card__photo" />
+      <img src={props.movie.Poster === "N/A" ? imageNotFoundPath : props.movie.Poster} alt={props.movie.Title} className={props.movie.Poster === "N/A" ? "card__notFound" : "card__photo"} />
       <h2 className="card__name" id="cardName">{props.movie.Title}</h2>
       <button className="card__info" onClick={getMovieData}>Learn more</button>
     </li>
--- src/components/ModalWithInfo.js
+++ src/components/ModalWithInfo.js
@@ -1,13 +1,13 @@
 //ФК, отвечающий за рендер модалки с расширенной инфой\
 import React from 'react';
-import imageNotFoundPath from "../images/notFound.png"
+import imageNotFoundPath from "../images/notAvailable.svg"
 
 function ModalWithInfo(props) {
 
   return (
     <div onClick={props.onClose} className={`info__wrapper ${props.isOpen ? 'info__wrapper_active' : ""}`}>
       <div className="info">
-        <img className="info__poster" src={props.data.Poster === "N/A" ? imageNotFoundPath : props.data.Poster} alt={props.data.Title} />
+        <img className={props.data.Poster === "N/A" ? "info__without-poster" : "info__poster"} src={props.data.Poster === "N/A" ? imageNotFoundPath : props.data.Poster} alt={props.data.Title} />
         <div className="info__text-section">
           <h2 className="info__caption">Director:</h2>
           <p name="infoDirector" className="info__text">{props.data.Director}</p>
